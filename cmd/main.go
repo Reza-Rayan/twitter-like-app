be2b7831027377b076915b5187fa83bf3034400a
reza-rayan <reza.hosseinzadeh@elanza.com>
2025-08-31 08:50:15 +0330

    remove Rest monitoring

--- cmd/main.go
+++ cmd/main.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"github.com/Reza-Rayan/twitter-like-app/config"
 	"github.com/Reza-Rayan/twitter-like-app/db"
-	"github.com/Reza-Rayan/twitter-like-app/middlewares"
 	"github.com/Reza-Rayan/twitter-like-app/routes"
 	"github.com/gin-gonic/gin"
 	"log"
@@ -21,14 +20,6 @@ func main() {
 	db.InitDB()
 	server := gin.Default()
 	routes.RegisterRoutes(server)
-
-	// Apply Prometheus middleware globally
-	server.Use(middlewares.PrometheusMiddleware())
-
-	// Conditionally expose /metrics based on config
-	if config.AppConfig.Monitoring.Enabled {
-		server.GET(config.AppConfig.Monitoring.Path, middlewares.PrometheusHandler())
-	}
 
 	server.Static("/uploads", "./uploads") // Serve files in the uploads folder at /uploads URL path
 
